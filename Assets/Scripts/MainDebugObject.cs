5000fca482affc9ce3ac37c4b8dbffa8915fbf59
Alexander Dubas <zx.local@outlook.com>
2015-12-24 00:47:37 +0200

    game started

---
+++ Assets/Scripts/MainDebugObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using DsFramework;
+
+
+namespace BsdGame
+{
+    [RequireComponent(typeof(DebugConsole))]
+    [RequireComponent(typeof(OnScreenLog))]
+    public class MainDebugObject : MonoBehaviour
+    {
+
+    }
+}
